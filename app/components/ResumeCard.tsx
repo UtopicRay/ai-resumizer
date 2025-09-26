e252fa2feba273f49848adbb9097732d79d55c4a
rayrm2021 <rayrm2021@gmail.com>
2025-09-26 00:59:30 +0000

    Complete homePage

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router";
+import ScoreCircle from "./CircularScore";
+
+export default function ResumeCard({ resume: { id, imagePath, companyName, jobTitle, feedback,resumePath } }: { resume: Resume }) {
+    return (
+        <Link to={`/resume/${id}`} className="resume-card fade-in animate-in duration-100">
+            <div className="resume-card-header">
+                <div className="flex flex-col gap-2">
+                    <h2 className="!text-black text-xl font-bold">{companyName}</h2>
+                    <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
+                </div>
+                <div className="flex shrink-0">
+                    <ScoreCircle score={feedback.overallScore}></ScoreCircle>
+                </div>
+            </div>
+            <div className="gradient-border animate-in fade-in duration-1000">
+                <div className="w-100 h-100">
+                <img src={imagePath} alt="resume" className="max-sm:h-[200px] w-full h-[350px] object-cover object-top" />
+                </div>
+            </div>
+        </Link>
+    )
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,9 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constant";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +11,22 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-cover bg-[url('/images/bg-main.svg')]">
+      <Navbar></Navbar>
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+        </div>
+      </section>
+      {resumes.length > 0 && (
+        <div className="resumes-section py-16">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
